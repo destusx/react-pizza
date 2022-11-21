08601fe9089bdddaffdea5d80f7fcb9df5690524
roman <adnicepvp@gmail.com>
2022-11-21 14:49:02 +0200

    create pizza project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./scss/components/Header";
+import Categories from "./scss/components/Categories";
+import Sort from "./scss/components/Sort";
+import PizzaBlock from "./scss/components/PizzaBlock";
+import "./scss/app.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div class="wrapper">
+            <Header />
+            <div class="content">
+                <div class="container">
+                    <div class="content__top">
+                        <Categories />
+                        <Sort />
+                    </div>
+                    <h2 class="content__title">Все пиццы</h2>
+                    <div class="content__items">
+                        <PizzaBlock title="Мексиканская" price={500} />
+                        <PizzaBlock />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
